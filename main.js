781c7571dca18e92641a2599e8a5c7c04578d184
Pierre Nogaro <pierre.nogaro@gmail.com>
2023-11-01 15:53:56 +0100

    fin

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-// SANS FETCH
+const url = 'https://blockchain.info/ticker';
 
-const url = "https://blockchain.info/ticker"
+function recupererPrix() {
+    // Créer une requête
+    let requete = new XMLHttpRequest(); // Créer un objet
+    requete.open('GET', url); // Premier paramètre GET / POST, deuxième paramètr : url
+    requete.responseType = 'json'; // Nous attendons du JSON
+    requete.send(); // Nous envoyons notre requête
 
-// créer une requête :
+    // Dèss qu'on reçoit une réponse, cette fonction est executée
+    requete.onload = function() {
+        if (requete.readyState === XMLHttpRequest.DONE) {
+            if (requete.status === 200) {
+                let reponse = requete.response; // on stock la réponse
+                let prixEnEuros = reponse.EUR.last;
+                document.querySelector('#price_label').textContent = prixEnEuros;
+            }
+            else {
+                alert('Un problème est intervenu, merci de revenir plus tard.');
+            }
+        }
+    }
+    console.log('Prix actualisé');
+}
 
-let requete = new XMLHttpRequest() // créer objet
-requete.open ("GET", "url") // premier param GET / POST
-
-requete.responseType = 'json' // j'attend du JSON
-requete.send() // j'envoie la requête
-
+setInterval(recupererPrix, 1000);
